2e56034e26f64b191e57bc723e69e5521b5bdf45
madvin <madvin@abv.bg>
2025-11-04 19:16:55 +0200

    add proper main.html

--- picureOfTheDay/src/components/Footer.jsx
+++ picureOfTheDay/src/components/Footer.jsx
@@ -3,6 +3,12 @@ export default function Footer() {
     <footer>
       <div>
         <h2>The picture of the day</h2>
+        <p>by NASA</p>
+        <div>
+          <button>
+            <i className='fa-solid fa circle-info'></i>
+          </button>
+        </div>
       </div>
     </footer>
   );
