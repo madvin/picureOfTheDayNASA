ad90932a93ae291173ec80cd65503f938eb30989
madvin <madvin@abv.bg>
2025-11-08 21:17:46 +0200

    add modal to show only if is clicked

--- picureOfTheDay/src/App.jsx
+++ picureOfTheDay/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 import Footer from "./components/Footer"
 
 function App() {
 
+  const [showModal, setShowModal] = useState(false);
+
+
   return (
     <> 
       <Main/>
-      <SideBar/>
+      {showModal && <SideBar> EMO </SideBar>}
+    
       <Footer/>
     </>
   )
